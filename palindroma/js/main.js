6fca8678c8608a81b5139e80c4a18aa60f19bd72
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-26 15:25:25 +0200

    aggiunta struttura base esercizi

---
+++ palindroma/js/main.js
@@ -0,0 +1,13 @@
+/*
+Palindroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+Passaggi da fare:
+-creare una variabile per chiedere all'utente una parola da inserire
+-creare un array in cui pushare la parola dell'utente (questa parte è opzionale)
+-creare la nostra funzione (function()) per capire se la parola inserita dall'utente è palindroma.
+-se è palindroma stampare il risultato
+-se non è palindroma riportare un errore
+*/
+
