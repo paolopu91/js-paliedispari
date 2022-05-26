40eba5c9ac2930fd5077c62e216307ab7c73a524
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-26 15:49:23 +0200

    Parola Palindroma completato

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -11,3 +11,26 @@ Passaggi da fare:
 -se non è palindroma riportare un errore
 */
 
+// chiedi all'utente di inserire una parola
+const askUtente = prompt("Inserisci una parola Palindroma");
+
+//Ho creato un allert per spiegare all'utente cosa significa palindrom
+alert("Si definisce palindrom una parola che si può leggere da sinistra verso destra e da destra verso sinistra")
+console.log(askUtente);
+
+// creare il mio array per inserire all'interno le parole palindrome
+const contenitorePalindromo = [];
+
+// la mia funzione dovrà verificare che la parola inserita dall'utente sia Palindroma
+function parolaPalindroma (parola){
+
+    let parolaInversa = parola.split("").reverse().join("");
+
+    if(parola == parolaInversa){
+        console.log('La parola è Palindroma,bravo!')
+    } else{
+        console.log('La parola non è Palindroma, riprova!')
+    }
+    return parolaInversa;
+}
+console.log(parolaPalindroma(askUtente));
